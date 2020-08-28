53f771f0ef2d2d5e822b6ec618ffb084595a8c02
Jhonathan Adriano <70270385+jhonathanAdriano@users.noreply.github.com>
2020-08-27 21:29:40 -0300

    Conexão com Banco de Dados

---
+++ Aula 3/corretora/corretora/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace corretora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAnoNascimento = new System.Windows.Forms.TextBox();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.dataGridViewSegurado = new System.Windows.Forms.DataGridView();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAnoNascimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSegurado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Ano Nascimento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Cidade:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nome:";
+            // 
+            // textBoxAnoNascimento
+            // 
+            this.textBoxAnoNascimento.Location = new System.Drawing.Point(397, 28);
+            this.textBoxAnoNascimento.Name = "textBoxAnoNascimento";
+            this.textBoxAnoNascimento.Size = new System.Drawing.Size(85, 20);
+            this.textBoxAnoNascimento.TabIndex = 10;
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.Location = new System.Drawing.Point(238, 28);
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(153, 20);
+            this.textBoxCidade.TabIndex = 9;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(13, 27);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(219, 20);
+            this.textBoxNome.TabIndex = 8;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(488, 25);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 7;
+            this.buttonSalvar.Text = "Adicionar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // dataGridViewSegurado
+            // 
+            this.dataGridViewSegurado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSegurado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnNome,
+            this.ColumnCidade,
+            this.ColumnAnoNascimento});
+            this.dataGridViewSegurado.Location = new System.Drawing.Point(12, 53);
+            this.dataGridViewSegurado.Name = "dataGridViewSegurado";
+            this.dataGridViewSegurado.Size = new System.Drawing.Size(549, 238);
+            this.dataGridViewSegurado.TabIndex = 14;
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "ID";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.Width = 50;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.Width = 200;
+            // 
+            // ColumnCidade
+            // 
+            this.ColumnCidade.HeaderText = "Cidade";
+            this.ColumnCidade.Name = "ColumnCidade";
+            this.ColumnCidade.Width = 150;
+            // 
+            // ColumnAnoNascimento
+            // 
+            this.ColumnAnoNascimento.HeaderText = "Ano Nascimento";
+            this.ColumnAnoNascimento.Name = "ColumnAnoNascimento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 303);
+            this.Controls.Add(this.dataGridViewSegurado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAnoNascimento);
+            this.Controls.Add(this.textBoxCidade);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.buttonSalvar);
+            this.Name = "Form1";
+            this.Text = "Cadastro Segurado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSegurado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAnoNascimento;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.DataGridView dataGridViewSegurado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAnoNascimento;
+    }
+}
+
---
+++ Aula 3/corretora/corretora/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace corretora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "corretora";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+                //MessageBox.Show("Conexão Aberta!");
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "INSERT INTO segurado (nomeSegurado,cidadeSegurado,anoNascimentoSegurado) " +
+                    "VALUES('" + textBoxNome.Text + "', '" + textBoxCidade.Text + "', '" + textBoxAnoNascimento.Text + "')";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Inserido com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                textBoxNome.Text = "";
+                textBoxCidade.Text = "";
+                textBoxAnoNascimento.Text = "";
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                atualizarGrid();
+            }
+
+            private void atualizarGrid()
+            {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "corretora";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * from segurado"; //Traz todo mundo da tabela segurado
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridViewSegurado.Rows.Clear();//FAZ LIMPAR A TABELA
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridViewSegurado.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//CIDADE
+                    row.Cells[3].Value = reader.GetString(3);//ANO NASCIMENTO
+                    dataGridViewSegurado.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+                realizaConexacoBD.Close();
+                //MessageBox.Show("Removido com sucesso"); ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
