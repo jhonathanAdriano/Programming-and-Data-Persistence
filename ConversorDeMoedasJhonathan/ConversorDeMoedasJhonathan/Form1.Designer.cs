ebbacb403e0a3048758beba562cd4103afae8f81
Jhonathan Adriano <70270385+jhonathanAdriano@users.noreply.github.com>
2020-08-27 21:28:14 -0300

    Conversor de Moedas

---
+++ ConversorDeMoedasJhonathan/ConversorDeMoedasJhonathan/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ConversorDeMoedasJhonathan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.valorR = new System.Windows.Forms.TextBox();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEuro = new System.Windows.Forms.TextBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VALOR PARA CONVERSÃO R$";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 7;
+            // 
+            // valorR
+            // 
+            this.valorR.Location = new System.Drawing.Point(203, 2);
+            this.valorR.Name = "valorR";
+            this.valorR.Size = new System.Drawing.Size(100, 20);
+            this.valorR.TabIndex = 2;
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.ForeColor = System.Drawing.Color.Red;
+            this.btnConverter.Location = new System.Drawing.Point(110, 35);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(94, 23);
+            this.btnConverter.TabIndex = 8;
+            this.btnConverter.Text = "CONVERTER";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "EURO:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "DOLAR:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "PESO:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtEuro
+            // 
+            this.txtEuro.Location = new System.Drawing.Point(91, 72);
+            this.txtEuro.Name = "txtEuro";
+            this.txtEuro.Size = new System.Drawing.Size(100, 20);
+            this.txtEuro.TabIndex = 12;
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(91, 98);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(100, 20);
+            this.txtDolar.TabIndex = 13;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(91, 125);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 157);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.txtEuro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.valorR);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CONVERSOR DE MOEDAS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox valorR;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEuro;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtPeso;
+    }
+}
+
---
+++ ConversorDeMoedasJhonathan/ConversorDeMoedasJhonathan/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorDeMoedasJhonathan
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double rl, rlEuro, rlDolar, rlPeso;
+            rl = Convert.ToDouble(valorR.Text);
+            rlEuro = rl * 0.20;
+            rlDolar = rl * 0.17;
+            rlPeso = rl * 0.5;
+            txtEuro.Text = Convert.ToString(rlEuro);
+
+        }
