42c809420c0093fcf5e2e75a2b5af88e123eda43
Jhonathan Adriano <70270385+jhonathanAdriano@users.noreply.github.com>
2020-08-27 21:18:15 -0300

    Botão Multiplicar

---
+++ Aula 1/aula01EAD/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula01EAD
+{
+    public partial class Form1 : Form
+    {
+        double valor = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            valor = double.Parse(tbTexto.Text) + valor;
+            tbResultado.Text = valor.ToString();
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            valor = valor - double.Parse(tbTexto.Text);
+            tbResultado.Text = valor.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valor = valor / double.Parse(tbTexto.Text);
+            tbResultado.Text = valor.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            valor = valor * double.Parse(tbTexto.Text);
+            tbResultado.Text = valor.ToString();
+        }
+    }
+}
